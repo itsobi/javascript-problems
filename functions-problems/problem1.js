c9c44c01a4611591cf2ced5373c559dbe59e5854
O <o@obis-mbp.lan>
2021-07-25 23:31:09 -0500

    Added a step by step process in how to solve JavaScript Problem 1

---
+++ functions-problems/problem1.js
@@ -0,0 +1,15 @@
+// Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.
+// Hint: use the "spilt" method and a "for" loop.
+
+/*
+Step 1: Create function findLongestWord and parameter (str)
+Step 2: Create variable for array of words using the split method. var str = str.split(" ");
+Step 3: Create variable that will hold the longest word. var longestWord = 0;
+Step 4: Create for loop to iterate through array of words. for (var i = 0; i < str.length; i++);
+Step 5: Create a conditional if statement to compare the longest word with each word in the array. 
+Step 6: In the if statement code block, set the longest word = the biggest word in the array
+Step 7: return the value of the single word.
+Step 8: Log the function to the console.
+*/
+
+
