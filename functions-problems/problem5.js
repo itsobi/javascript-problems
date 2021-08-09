2430af13c8b67ba9888b9e32bbe64ee4432f931a
O <o@obis-mbp.lan>
2021-08-09 10:01:46 -0500

    Turned JS Problem 5 into a function

--- functions-problems/problem5.js
+++ functions-problems/problem5.js
@@ -9,21 +9,23 @@
 
 
 
-
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  }
-  
-  else if (i % 3 === 0) {
-    console.log("Fizz");
-  }
-  
-  else if (i % 5 === 0) {
-    console.log("Buzz");
-  }
-  
-  else {
-    console.log(i)
+function fizzBuzz(number) {
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    }
+    else if (i % 3 === 0) {
+      console.log("Fizz");
+    }
+    else if (i % 5 === 0) {
+      console.log("Buzz");
+    }
+    else {
+      console.log(i);
+    }
   }
 }
+
+let one = 100;
+
+fizzBuzz(one);
