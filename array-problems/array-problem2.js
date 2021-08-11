af229e5df249f863efcd078017eb2e6330646dc5
O <o@obis-mbp.lan>
2021-08-11 16:29:57 -0500

    Arrays: Problem 2 Solution

---
+++ array-problems/array-problem2.js
@@ -0,0 +1,6 @@
+// 1: Create variable cars, set equal to array
+// 2: log cars.join() to console to turn array into string.
+
+let cars = ["Toyota", "Honda", "Chevy", "Ford"];
+
+console.log(cars.join())
