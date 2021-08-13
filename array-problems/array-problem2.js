e5c6c0d5de87df3b0f5a8199a21cd1d9c9395658
O <o@obis-mbp.lan>
2021-08-13 11:00:45 -0500

    Added function to problem, and added code inside function code block

--- array-problems/array-problem2.js
+++ array-problems/array-problem2.js
@@ -1,6 +1,12 @@
 // 1: Create variable cars, set equal to array
-// 2: log cars.join() to console to turn array into string.
+// 2: Create function joinCars. In block code log cars.join
+// 3: call the function joinCars
 
 let cars = ["Toyota", "Honda", "Chevy", "Ford"];
 
-console.log(cars.join())
+function joinCars() {
+  console.log(cars.join())
+}
+
+
+joinCars();
