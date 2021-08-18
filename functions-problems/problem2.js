74030d1ac1f65b258061dcf3c7cbcc0d0fc0bafa
Obi Obialo <obiobialo@aol.com>
2021-08-18 12:51:48 -0500

    Functions: Problem 2 Solution

--- functions-problems/problem2.js
+++ functions-problems/problem2.js
@@ -1,26 +1,24 @@
-/*
-Step 1: Create function vowelCounter with parameter of (str)
-Step 2: Create counter set equal to 0. let counter = 0
-Step 3: Create variable of the vowels. let vowels = "aeiou" to assign vowels to the variable
-Step 4: Crete for loop to iterate through sentence to find vowels.
-Step 5: Create conditional if statement using .includes & .toLowerCase to turn all the words in the sentence to lower case.
-Step 6: Return counter. To add more flare to it, add words between quotes + counter + to return a words with the returned value.
-*/
+// 1: Create function vowelCounter with parameter of (str)
+// 2: In function code block, create variable vowels = "aeiou"
+// and variable counter = 0 to hold numeric count of vowels in string passed in
+// 3: Create for loop through length of str
+// 4: In code block create if statement, if vowels.indexOf(str[i].toLowerCase()) is greater than -1
+// increase counter by 1 (counter++)
+// 5: return counter
 
 function vowelCounter(str) {
-  
-  let arrayOfLetters = str.split("");
   let vowels = "aeiou";
   let counter = 0;
-
   
-  for (let i = 0; i < arrayOfLetters.length; i++) {
-    if (vowels.includes(arrayOfLetters[i].toLowerCase())) {
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.indexOf(str[i].toLowerCase()) > -1) {
       counter++
     }
   }
   
-  return "This sentence has "  + counter +  " vowels in it!";
+  
+  return counter;
 }
 
-console.log(vowelCounter("How many vowels are in this sentence?"));
+
+console.log(vowelCounter("How many vowels are in this string?"));
