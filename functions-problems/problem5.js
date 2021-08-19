08d897597e7714c189a4e23f5aebcfa829805cd8
Obi Obialo <obiobialo@aol.com>
2021-08-19 15:34:35 -0500

    Made comments fit on screen

--- functions-problems/problem5.js
+++ functions-problems/problem5.js
@@ -1,4 +1,6 @@
-// Write a JavaScript program that iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers that are multiples of both three and five print "FizzBuzz".
+// Write a JavaScript program that iterates the integers from 1 to 100. But for multiples of three print "Fizz" 
+// instead of the number and for the multiples of five print "Buzz". 
+// For numbers that are multiples of both three and five print "FizzBuzz".
 
 
 
