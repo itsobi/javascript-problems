5838d7194357abe98039bc1f4aff14c550a9a1d2
Obi Obialo <obiobialo@aol.com>
2021-08-19 15:32:54 -0500

    Functions: Problem 5 Solution

--- functions-problems/problem5.js
+++ functions-problems/problem5.js
@@ -1,16 +1,11 @@
-// Step 1: Create for loop to iterate from 1-100
-// Step 2: Create if statement using modulus/remainder operator. Include 3 & 5 in first if statement so there are not two tests for the same condition
-// Step 4: In the if statement code block, console.log "FizzBuzz" so the console will print when multiples of 3 & 5 occur.
-// Step 5: Create else if statement to be executed if the first statement is false. (i % 3 === 0) in that code block log "Fizz" to the console.
-// Step 6: Create another else if to executed if the first 2 conditions are false. (i % 5 === 0). In that code block log "Buzz" to the console.
-// Step 7: Create else statement that will be executed if all other conditions are false. log (i) to the console which will be every number that is not a multiple of 3, 5, or both.
-
+// Write a JavaScript program that iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers that are multiples of both three and five print "FizzBuzz".
 
 
 
 
 function fizzBuzz(number) {
-  for (let i = 1; i < number; i++) {
+  
+  for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("FizzBuzz");
     }
@@ -26,6 +21,4 @@ function fizzBuzz(number) {
   }
 }
 
-let one = 100;
-
-fizzBuzz(one);
+fizzBuzz();
