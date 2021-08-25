4888ec90aff3231b72a3cf5d1495bedfda831cd5
Your Namef <obiobialo@aol.com>
2021-08-25 15:40:16 -0500

    Recursion: Problem 2 Solution

--- recursion-problems/recursion-problem2.js
+++ recursion-problems/recursion-problem2.js
@@ -1,10 +1,16 @@
-function sum(arr) {
-    let count = 0
-    
-    for (let i = 0; i < arr.length; i++) {
-      count = count + arr[i];
-    }
-    console.log(count);
+//Write a JavaScript program to compute the sum of an array of integers.
+
+
+
+const sum = (array) => {
+  if (array.length === 0) {
+    return 0
   }
-  
-  sum([5, 1, 45, 6, 8]);
+  else {
+    return array[0] + sum(array.slice(1))
+  }
+}
+
+const sumValue = sum([5, 1, 45, 6, 8]);
+
+console.log(sumValue);
