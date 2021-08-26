863e9e0e4a1f034dca251df3cd1ad043874b34ba
Your Namef <obiobialo@aol.com>
2021-08-26 11:19:42 -0500

    Cleaned up code, so function would take words, as well as numbers

--- array-problems/array-problem4.js
+++ array-problems/array-problem4.js
@@ -1,18 +1,20 @@
 //Write a JavaScript program to remove duplicate items from an array (consider case sensitivity).
 
-
-
 function removeDuplicate(arr) {
   let newArr = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr.indexOf(i) > -1) {
-      newArr.push(i);
+  
+  for (let i = 0; i <arr.length; i++) {
+    if (newArr.indexOf(arr[i]) === -1) {
+      newArr.push(arr[i]);
     }
   }
-  return newArr;
+  return newArr
 }
 
-console.log(removeDuplicate([1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6]));
+
+
+console.log(removeDuplicate(["Jeep", "Jeep", "Volvo", "Toyota"]));
+
+
 
 
